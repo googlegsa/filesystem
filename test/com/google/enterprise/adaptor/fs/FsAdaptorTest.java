googlegsa/filesystem

commit 5deeecc3bf069701bcb0e33428ee5f0bbc19a309

NioFileDelegate.getPath() rethrows InvalidPathException as IOException
NioFileDelegate.getPath() catches InvalidPathException, a RuntimeException,
and rethrows it as an IOException to be handled by the adaptor.

Code Review: http://codereview.appspot.com/247400043

src/com/google/enterprise/adaptor/fs/NioFileDelegate.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -36,7 +37,11 @@ abstract class NioFileDelegate implements FileDelegate {
 
   @Override
   public Path getPath(String pathname) throws IOException {
-    return Paths.get(pathname);
+    try {
+      return Paths.get(pathname);
+    } catch (InvalidPathException e) {
+      throw new IOException("Invalid path: " + pathname, e);
+    }
   }
 
   @Override

commit 7bac1d58e82efda91dc4938b017a34082a4dfb17

Fix b/15940781 Revert commit 5deeecc and handle InvalidPathExceptions explicitly
Commit 5deeecc: "NioFileDelegate.getPath() rethrows InvalidPathException
as IOException":
https://github.com/googlegsa/filesystem/commit/5deeecc3bf069701bcb0e33428ee5f0bbc19a309
broke a previous fix for b/15940781: https://codereview.appspot.com/106580044/

This change reverts the 5deeecc and restores FsAdaptor.getDocContent()
ability to handle InvalidPathExceptions. The change adds InvalidPathException
handling in other locations that called getPath, and additional unit tests
to guard against further breakage.

Code Review: http://codereview.appspot.com/282780043

src/com/google/enterprise/adaptor/fs/FileDelegate.java
@@ -20,6 +20,7 @@ import com.google.enterprise.adaptor.DocId;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.DirectoryStream;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.attribute.AclFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -33,9 +34,8 @@ interface FileDelegate {
    *
    * @param pathname the path string
    * @return the real Path
-   * @throws IOException
    */
-  Path getPath(String pathname) throws IOException;
+  Path getPath(String pathname) throws IOException, InvalidPathException;
 
   /**
    * Returns {@code true} if the specified path represents

src/com/google/enterprise/adaptor/fs/FsAdaptor.java
@@ -420,7 +420,13 @@ public class FsAdaptor extends AbstractAdaptor {
       throw new InvalidConfigurationException("The configuration value "
           + CONFIG_SRC + " is empty. Please specify a valid root path.");
     }
-    startPaths = getStartPaths(sources, config.getValue(CONFIG_SRC_SEPARATOR));
+    try {
+      startPaths =
+          getStartPaths(sources, config.getValue(CONFIG_SRC_SEPARATOR));
+    } catch (InvalidPathException e) {
+      throw new InvalidConfigurationException(CONFIG_SRC
+          + " contains an invalid start path. " + e.getMessage());
+    }
 
     builtinPrefix = config.getValue(CONFIG_BUILTIN_PREFIX);
     log.log(Level.CONFIG, "builtinPrefix: {0}", builtinPrefix);
@@ -545,7 +551,7 @@ public class FsAdaptor extends AbstractAdaptor {
   /** Parses the collection of startPaths from the supplied sources. */
   @VisibleForTesting
   Set<Path> getStartPaths(String sources, String separator)
-      throws IOException {
+      throws IOException, InvalidPathException {
     if (separator.isEmpty()) {
       // No separator implies a single startPath.
       return ImmutableSet.of(delegate.getPath(sources));
@@ -1524,7 +1530,7 @@ public class FsAdaptor extends AbstractAdaptor {
               "Authorization decision {0} for user {1} and doc {2}.",
               new Object[]{decision, user, id});
           result.put(id, decision);
-        } catch (IOException ioe) {
+        } catch (InvalidPathException | IOException ioe) {
           log.log(Level.WARNING, "Could not get ACL.", ioe);
           result.put(id, AuthzStatus.INDETERMINATE);
         }

src/com/google/enterprise/adaptor/fs/NioFileDelegate.java
@@ -36,12 +36,9 @@ import java.nio.file.attribute.FileTime;
 abstract class NioFileDelegate implements FileDelegate {
 
   @Override
-  public Path getPath(String pathname) throws IOException {
-    try {
-      return Paths.get(pathname);
-    } catch (InvalidPathException e) {
-      throw new IOException("Invalid path: " + pathname, e);
-    }
+  public Path getPath(String pathname)
+      throws IOException, InvalidPathException {
+    return Paths.get(pathname);
   }
 
   @Override

test/com/google/enterprise/adaptor/fs/FsAdaptorTest.java
@@ -45,6 +45,7 @@ import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.DirectoryStream;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.AclFileAttributeView;
@@ -125,6 +126,21 @@ public class FsAdaptorTest {
   @Test
   public void testAdaptorInitNoSourcePath() throws Exception {
     config.overrideKey("filesystemadaptor.src", "");
+    thrown.expect(InvalidConfigurationException.class);
+    adaptor.init(context);
+  }
+
+  @Test
+  public void testAdaptorInitInvalidPathException() throws Exception {
+    delegate = new MockFileDelegate(root) {
+      @Override
+      public Path getPath(String pathname) throws IOException {
+        throw new InvalidPathException(pathname, "The path is invalid");
+      }
+    };
+    adaptor = new FsAdaptor(delegate);
+
+    config.overrideKey("filesystemadaptor.src", "invalid path");
     thrown.expect(InvalidConfigurationException.class);
     adaptor.init(context);
   }
@@ -520,10 +536,26 @@ public class FsAdaptorTest {
   }
 
   @Test
-  public void testGetDocContentInvalidPath() throws Exception {
+  public void testGetDocContentInvalidPathException() throws Exception {
+    // Create a file called "invalid", so it would actually be found
+    // if it was not hit with InvalidPathException.
+    MockFile invalid = new MockFile("invalid");
+    root.addChildren(invalid);
+    delegate = new MockFileDelegate(root) {
+      @Override
+      public Path getPath(String pathname) throws IOException {
+        if (pathname.contains("invalid")) {
+          throw new InvalidPathException(pathname, "The path is invalid");
+        } else {
+          return super.getPath(pathname);
+        }
+      }
+    };
+    adaptor = new FsAdaptor(delegate);
     adaptor.init(context);
     MockResponse response = new MockResponse();
-    adaptor.getDocContent(new MockRequest(new DocId("")), response);
+    adaptor.getDocContent(new MockRequest(new DocId(invalid.getPath())),
+                          response);
     assertTrue(response.notFound);
   }
 

test/com/google/enterprise/adaptor/fs/MockFileDelegate.java
@@ -15,7 +15,6 @@
 package com.google.enterprise.adaptor.fs;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.enterprise.adaptor.AsyncDocIdPusher;
 import com.google.enterprise.adaptor.DocId;
@@ -24,7 +23,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.DirectoryStream;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.AclFileAttributeView;
@@ -77,10 +75,6 @@ class MockFileDelegate implements FileDelegate {
 
   @Override
   public Path getPath(String pathname) throws IOException {
-    if (Strings.isNullOrEmpty(pathname)) {
-      throw new InvalidPathException(pathname,
-                                     "pathname cannot be null or empty");
-    }
     return Paths.get(pathname);
   }
 

test/com/google/enterprise/adaptor/fs/NioFileDelegateTest.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -74,6 +75,13 @@ public class NioFileDelegateTest {
 
   private void testGetPath(String pathname) throws Exception {
     assertTrue(Paths.get(pathname).equals(delegate.getPath(pathname)));
+  }
+
+  @Test
+  public void testGetPathInvalidPathException() throws Exception {
+    TestHelper.assumeOsIsWindows();
+    thrown.expect(InvalidPathException.class);
+    testGetPath("\\\\host\\");  // UNC path with no share.
   }
 
   @Test