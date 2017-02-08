googlegsa/filesystem

commit 06e4ade092d653a0bfc203d944a5dd4c1eed7b14

Fix case for mime type extensions
Code review: https://codereview.appspot.com/297450043/

src/com/google/enterprise/adaptor/fs/FsAdaptor.java
@@ -15,6 +15,7 @@
 package com.google.enterprise.adaptor.fs;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.ENGLISH;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
@@ -1177,7 +1178,7 @@ public class FsAdaptor extends AbstractAdaptor {
     String fileName = doc.toString();
     int pos = fileName.lastIndexOf(".");
     if (pos != -1) {
-      String extension = fileName.substring(pos + 1);
+      String extension = fileName.substring(pos + 1).toLowerCase(ENGLISH);
       String mimetype = mimeTypeProperties.getProperty(extension);
       if (mimetype != null) {
         return mimetype.trim();
@@ -1195,6 +1196,10 @@ public class FsAdaptor extends AbstractAdaptor {
     try (BufferedReader fileInput =
         Files.newBufferedReader(Paths.get(MIME_TYPE_PROP_FILENAME), UTF_8)) {
       properties.load(fileInput);
+      for (String key : properties.stringPropertyNames()) {
+        mimeTypeProperties.setProperty(key.toLowerCase(ENGLISH),
+            properties.getProperty(key));
+      }
     } catch (FileNotFoundException e1) {
       log.log(Level.FINE, "No {0} file found", MIME_TYPE_PROP_FILENAME);
     } catch (IOException e) {
@@ -1261,7 +1266,8 @@ public class FsAdaptor extends AbstractAdaptor {
   @VisibleForTesting
   protected void setMimeTypeProperties(Properties prop) {
     for (String key : prop.stringPropertyNames()) {
-      mimeTypeProperties.setProperty(key, prop.getProperty(key));
+      mimeTypeProperties.setProperty(key.toLowerCase(ENGLISH),
+          prop.getProperty(key));
     }
   }
 

test/com/google/enterprise/adaptor/fs/FsAdaptorTest.java
@@ -742,6 +742,30 @@ public class FsAdaptorTest {
         null, null);
   }
 
+  @Test
+  public void testDocMimeTypeFromLocalMapMixedCaseExtension() throws Exception {
+    String mimetype = "application/vnd.ms-excel.sheet.macroEnabled.12";
+    testGetDocMimeType(getMockFile("test.Xlsm"), mimetype, null);
+  }
+
+  @Test
+  public void testDocMimeTypeFromPropertiesMixedCaseExtension()
+      throws Exception {
+    Properties props = new Properties();
+    String mimetype = "presentation format";
+    props.setProperty("ppt", mimetype);
+    testGetDocMimeType(getMockFile("test.Ppt"), mimetype, props);
+  }
+
+  @Test
+  public void testDocMimeTypeFromPropertiesMixedCaseProperty()
+      throws Exception {
+    Properties props = new Properties();
+    String mimetype = "presentation format";
+    props.setProperty("Ppt", mimetype);
+    testGetDocMimeType(getMockFile("test.ppt"), mimetype, props);
+  }
+
   private MockFile getMockFile(String fname) throws Exception {
     MockFile file = new MockFile(fname);
     root.addChildren(file);

commit 462a4a20dc773166da2523cc5da0417248f2f03b

Fix b/34961349: Using mime-type.properties file crashes with NPE
This change fixes a NullPointerException introduced in commit 06e4ade.
Also properly handle NIO NoSuchFileException in loadMimeTypeProperties.
Added unit tests for loadMimeTypeProperties method.

Code Review: http://codereview.appspot.com/318490043

src/com/google/enterprise/adaptor/fs/FsAdaptor.java
@@ -280,11 +280,10 @@ public class FsAdaptor extends AbstractAdaptor {
   private static final String CONFIG_SEARCH_RESULTS_GO_TO_REPO
        = "filesystemadaptor.searchResultsLinkToRepository";
 
-  /** Properties filename to specify mime types. */
-  private static final String MIME_TYPE_PROP_FILENAME = "mime-type.properties";
-
-  /* mime type mapping */
-  private static final Properties mimeTypeProperties = getMimeTypes();
+  /** MIME type mapping. The format of the optional mime-type.properties file
+   *  is filename extensions as keys and MIME types as values. */
+  private static final Properties mimeTypeProperties =
+      getMimeTypes(Paths.get("mime-type.properties"));
 
   /** Fragements used for creating the inherited ACL named resources. */
   private static final String ALL_FOLDER_INHERIT_ACL = "allFoldersAcl";
@@ -1204,27 +1203,32 @@ public class FsAdaptor extends AbstractAdaptor {
 
   /**
    * Load mime types from properties file.
+   * @param userMimeType Path of the optional mime-type.properties file.
+   * @param defaults The default file extension to mime type map properties.
    * @return a Properties.
    */
-  private static Properties loadMimeTypeProperties(Properties props) {
-    Properties properties = new Properties(props);
+  @VisibleForTesting
+  static Properties loadMimeTypeProperties(Path userMimeTypes,
+      Properties defaults) {
+    Properties properties = new Properties(defaults);
     try (BufferedReader fileInput =
-        Files.newBufferedReader(Paths.get(MIME_TYPE_PROP_FILENAME), UTF_8)) {
-      properties.load(fileInput);
-      for (String key : properties.stringPropertyNames()) {
-        mimeTypeProperties.setProperty(key.toLowerCase(ENGLISH),
-            properties.getProperty(key));
+         Files.newBufferedReader(userMimeTypes, UTF_8)) {
+      Properties overrides = new Properties();
+      overrides.load(fileInput);
+      for (String key : overrides.stringPropertyNames()) {
+        properties.setProperty(key.toLowerCase(ENGLISH),
+            overrides.getProperty(key).trim());
       }
-    } catch (FileNotFoundException e1) {
-      log.log(Level.FINE, "No {0} file found", MIME_TYPE_PROP_FILENAME);
+    } catch (FileNotFoundException | NoSuchFileException e1) {
+      log.log(Level.FINE, "No {0} file found", userMimeTypes);
+      return defaults;
     } catch (IOException e) {
-      log.log(Level.FINE, "IOException reading {0} file",
-          MIME_TYPE_PROP_FILENAME);
+      log.log(Level.WARNING, "Error reading " + userMimeTypes + " file", e);
     }
     return properties;
   }
 
-  private static Properties getMimeTypes() {
+  private static Properties getMimeTypes(Path userMimeTypes) {
     Properties properties = new Properties();
 
     // mime type mapping from Microsoft Technet reference.
@@ -1274,7 +1278,7 @@ public class FsAdaptor extends AbstractAdaptor {
         + "macroEnabled.12");
 
     // get mime types from properties file.
-    return loadMimeTypeProperties(properties);
+    return loadMimeTypeProperties(userMimeTypes, properties);
   }
 
   /* Set mime type properties. */

test/com/google/enterprise/adaptor/fs/FsAdaptorTest.java
@@ -17,6 +17,7 @@ package com.google.enterprise.adaptor.fs;
 import static com.google.enterprise.adaptor.fs.AclView.GenericPermission.*;
 import static com.google.enterprise.adaptor.fs.AclView.group;
 import static com.google.enterprise.adaptor.fs.AclView.user;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.attribute.AclEntryFlag.*;
 import static java.nio.file.attribute.AclEntryPermission.*;
 import static java.nio.file.attribute.AclEntryType.*;
@@ -35,8 +36,12 @@ import com.google.enterprise.adaptor.GroupPrincipal;
 import com.google.enterprise.adaptor.InvalidConfigurationException;
 import com.google.enterprise.adaptor.UserPrincipal;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.FilterInputStream;
@@ -46,6 +51,7 @@ import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -107,6 +113,9 @@ public class FsAdaptorTest {
   }
 
   @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  @Rule
   public ExpectedException thrown = ExpectedException.none();
 
   private Path getPath(String path) {
@@ -115,6 +124,80 @@ public class FsAdaptorTest {
 
   private DocId getDocId(String path) throws IOException {
     return delegate.newDocId(getPath(path));
+  }
+
+  private Path writeMimeTypes(String content) throws IOException {
+    Path file = tempFolder.newFile("test-mime-types.properties").toPath();
+    Files.write(file, content.getBytes(UTF_8));
+    return file;
+  }
+
+  @Test
+  public void testLoadMimeTypesFileNotFound() throws Exception {
+    Path file = tempFolder.getRoot().toPath()
+        .resolve("non-existent-mime-types.properties");
+    Properties defaults = new Properties();
+    assertSame(defaults, FsAdaptor.loadMimeTypeProperties(file, defaults));
+  }
+
+  @Test
+  public void testLoadMimeTypesEmptyFile() throws Exception {
+    Path file = writeMimeTypes("");
+    Properties defaults = new Properties();
+    defaults.setProperty("ext1", "foo/bar");
+    defaults.setProperty("ext2", "foo/baz");
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals(new Properties(), mimeTypes);
+    // But the defaults should still work.
+    assertEquals("foo/bar", mimeTypes.getProperty("ext1"));
+    assertEquals("foo/baz", mimeTypes.getProperty("ext2"));
+  }
+
+  @Test
+  public void testLoadMimeTypesUniqueKey() throws Exception {
+    Path file = writeMimeTypes("ext1=foo/bar\n");
+    Properties defaults = new Properties();
+    defaults.setProperty("ext2", "foo/baz");
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals("foo/bar", mimeTypes.getProperty("ext1"));
+    assertEquals("foo/baz", mimeTypes.getProperty("ext2"));
+  }
+
+  @Test
+  public void testLoadMimeTypesMultipleKeys() throws Exception {
+    Path file = writeMimeTypes("ext1=foo/bar\next2=foo/baz\n");
+    Properties defaults = new Properties();
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals("foo/bar", mimeTypes.getProperty("ext1"));
+    assertEquals("foo/baz", mimeTypes.getProperty("ext2"));
+  }
+
+  @Test
+  public void testLoadMimeTypesOverrideDefaultValue() throws Exception {
+    Path file = writeMimeTypes("ext1=text/plain\n");
+    Properties defaults = new Properties();
+    defaults.setProperty("ext1", "foo/bar");
+    defaults.setProperty("ext2", "foo/baz");
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals("text/plain", mimeTypes.getProperty("ext1"));
+    assertEquals("foo/baz", mimeTypes.getProperty("ext2"));
+  }
+
+  @Test
+  public void testLoadMimeTypesToLowerExtensions() throws Exception {
+    Path file = writeMimeTypes("EXT1=text/plain\n");
+    Properties defaults = new Properties();
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals("text/plain", mimeTypes.getProperty("ext1"));
+    assertNull(mimeTypes.getProperty("EXT1"));
+  }
+
+  @Test
+  public void testLoadMimeTypesTrimValues() throws Exception {
+    Path file = writeMimeTypes("ext1=text/plain    \n");
+    Properties defaults = new Properties();
+    Properties mimeTypes = FsAdaptor.loadMimeTypeProperties(file, defaults);
+    assertEquals("text/plain", mimeTypes.getProperty("ext1"));
   }
 
   @Test